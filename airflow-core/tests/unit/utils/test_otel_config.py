c922745b820e0202107df483ef8ec9a3a769729e
Christos Bisias <christosbis@gmail.com>
2025-09-22 11:29:16 +0300

    add unit tests for config validations

--- airflow-core/tests/unit/utils/test_otel_config.py
+++ airflow-core/tests/unit/utils/test_otel_config.py
@@ -16,10 +16,49 @@
 # under the License.
 from __future__ import annotations
 
+import pytest
+
+from airflow.utils.otel_config import load_metrics_config, load_traces_config
+
+from tests_common.test_utils.config import env_vars
+
 
 def test_metrics_validation():
-    pass
+    with pytest.raises(OSError) as endpointExc:
+        load_metrics_config()
+
+    assert "OTEL_EXPORTER_OTLP_METRICS_ENDPOINT" in str(endpointExc.value)
+
+    url = "http://localhost:4318/v1/metrics"
+
+    with env_vars({"OTEL_EXPORTER_OTLP_METRICS_ENDPOINT": url}):
+        config = load_metrics_config()
+
+        assert config.endpoint == url
+        # Default values.
+        assert config.service_name == "Airflow"
+        assert config.protocol == "grpc"
+        assert not config.headers_raw
+        assert not config.resource_attributes_raw
+        # Check that the value is an int and not str.
+        assert config.interval != "60000"
+        assert config.interval == 60000
 
 
 def test_traces_validation():
-    pass
+    with pytest.raises(OSError) as endpointExc:
+        load_traces_config()
+
+    assert "OTEL_EXPORTER_OTLP_TRACES_ENDPOINT" in str(endpointExc.value)
+
+    url = "http://localhost:4318/v1/traces"
+
+    with env_vars({"OTEL_EXPORTER_OTLP_TRACES_ENDPOINT": url}):
+        config = load_traces_config()
+
+        assert config.endpoint == url
+        # Default values.
+        assert config.service_name == "Airflow"
+        assert config.protocol == "grpc"
+        assert not config.headers_raw
+        assert not config.resource_attributes_raw

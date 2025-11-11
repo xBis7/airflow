b8c0329aa08615559725801beff2e37588c2468d
Christos Bisias <christosbis@gmail.com>
2025-11-11 13:16:09 +0200

    integration test

--- airflow-core/tests/integration/otel/test_otel.py
+++ airflow-core/tests/integration/otel/test_otel.py
@@ -610,7 +610,7 @@ class TestOtelIntegration:
         "--daemon",
     ]
 
-    dags: dict[str, DAG] = {}
+    dags: dict[str, SerializedDAG] = {}
 
     @classmethod
     def setup_class(cls):
@@ -726,6 +726,81 @@ class TestOtelIntegration:
                 os.remove(self.control_file)
         except Exception as ex:
             log.error("Could not delete leftover control file '%s', error: '%s'.", self.control_file, ex)
+
+    def test_export_metrics(self, monkeypatch, celery_worker_env_vars, capfd, session):
+        # Metrics.
+        os.environ["AIRFLOW__METRICS__OTEL_ON"] = "True"
+        os.environ["AIRFLOW__METRICS__OTEL_HOST"] = "breeze-otel-collector"
+        os.environ["AIRFLOW__METRICS__OTEL_PORT"] = "4318"
+        os.environ["AIRFLOW__METRICS__OTEL_INTERVAL_MILLISECONDS"] = "5000"
+
+        if self.use_otel != "true":
+            os.environ["AIRFLOW__METRICS__OTEL_DEBUGGING_ON"] = "True"
+
+        celery_worker_process = None
+        scheduler_process = None
+        apiserver_process = None
+        try:
+            # Start the processes here and not as fixtures or in a common setup,
+            # so that the test can capture their output.
+            celery_worker_process, scheduler_process, apiserver_process = self.start_worker_and_scheduler1()
+
+            dag_id = "otel_test_dag"
+
+            assert len(self.dags) > 0
+            dag = self.dags[dag_id]
+
+            assert dag is not None
+
+            run_id = unpause_trigger_dag_and_get_run_id(dag_id=dag_id)
+
+            # Skip the span_status check.
+            wait_for_dag_run_and_check_span_status(
+                dag_id=dag_id, run_id=run_id, max_wait_time=90, span_status=None
+            )
+
+            # The ti span_status is updated while processing the executor events,
+            # which is after the dag_run state has been updated.
+            time.sleep(10)
+
+            task_dict = dag.task_dict
+            task_dict_ids = task_dict.keys()
+
+            for task_id in task_dict_ids:
+                # Skip the span_status check.
+                check_ti_state_and_span_status(
+                    task_id=task_id, run_id=run_id, state=State.SUCCESS, span_status=None
+                )
+
+        finally:
+            # Terminate the processes.
+            celery_worker_process.terminate()
+            celery_worker_process.wait()
+
+            celery_status = celery_worker_process.poll()
+            assert celery_status is not None, (
+                "The celery worker process status is None, which means that it hasn't terminated as expected."
+            )
+
+            scheduler_process.terminate()
+            scheduler_process.wait()
+
+            scheduler_status = scheduler_process.poll()
+            assert scheduler_status is not None, (
+                "The scheduler_1 process status is None, which means that it hasn't terminated as expected."
+            )
+
+            apiserver_process.terminate()
+            apiserver_process.wait()
+
+            apiserver_status = apiserver_process.poll()
+            assert apiserver_status is not None, (
+                "The apiserver process status is None, which means that it hasn't terminated as expected."
+            )
+
+        out, err = capfd.readouterr()
+        log.info("out-start --\n%s\n-- out-end", out)
+        log.info("err-start --\n%s\n-- err-end", err)
 
     @pytest.mark.execution_timeout(90)
     def test_dag_execution_succeeds(self, monkeypatch, celery_worker_env_vars, capfd, session):

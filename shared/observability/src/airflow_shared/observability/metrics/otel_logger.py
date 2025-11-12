c07319879a398b1eb552e6b5a131b2de89403a6d
Christos Bisias <christosbis@gmail.com>
2025-11-12 13:38:24 +0200

    reduce prometheus scrape intervals

--- shared/observability/src/airflow_shared/observability/metrics/otel_logger.py
+++ shared/observability/src/airflow_shared/observability/metrics/otel_logger.py
@@ -402,7 +402,7 @@ def get_otel_logger(
         PeriodicExportingMetricReader(
             OTLPMetricExporter(endpoint=metrics_endpoint),
             export_interval_millis=interval,  # type: ignore[arg-type]
-            export_timeout_millis=1000,
+            export_timeout_millis=3000,
         )
     ]
 

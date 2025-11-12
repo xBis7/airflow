4265da874215de2de41ea61b26e5120614e4b9dd
Christos Bisias <christosbis@gmail.com>
2025-11-12 08:08:59 +0200

    reduce export time

--- shared/observability/src/airflow_shared/observability/metrics/otel_logger.py
+++ shared/observability/src/airflow_shared/observability/metrics/otel_logger.py
@@ -402,6 +402,7 @@ def get_otel_logger(
         PeriodicExportingMetricReader(
             OTLPMetricExporter(endpoint=metrics_endpoint),
             export_interval_millis=interval,  # type: ignore[arg-type]
+            export_timeout_millis=30000,
         )
     ]
 
@@ -413,7 +414,7 @@ def get_otel_logger(
         MeterProvider(
             resource=resource,
             metric_readers=readers,
-            shutdown_on_exit=False,
+            shutdown_on_exit=True,
         ),
     )
 

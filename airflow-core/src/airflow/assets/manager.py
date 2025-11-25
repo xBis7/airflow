2ac44e56899516c65ebfb6a465baacbf63be33b4
Christos Bisias <christosbis@gmail.com>
2025-11-25 21:32:39 +0200

    fix stats and traces imports

--- airflow-core/src/airflow/assets/manager.py
+++ airflow-core/src/airflow/assets/manager.py
@@ -24,7 +24,6 @@ import structlog
 from sqlalchemy import exc, or_, select
 from sqlalchemy.orm import joinedload
 
-from airflow._shared.observability.stats import Stats
 from airflow.configuration import conf
 from airflow.listeners.listener import get_listener_manager
 from airflow.models.asset import (
@@ -37,6 +36,7 @@ from airflow.models.asset import (
     DagScheduleAssetReference,
     DagScheduleAssetUriReference,
 )
+from airflow.observability.stats import Stats
 from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.utils.sqlalchemy import get_dialect_name
 

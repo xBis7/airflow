04507f6d207c4fb7bac2dd10cf77b8271ebec00b
Christos Bisias <christosbis@gmail.com>
2026-07-01 14:43:10 +0300

    Add debug spans for the scheduler loop

--- airflow-core/src/airflow/models/dag.py
+++ airflow-core/src/airflow/models/dag.py
@@ -26,6 +26,7 @@ import pendulum
 import sqlalchemy as sa
 import structlog
 from dateutil.relativedelta import relativedelta
+from opentelemetry import trace
 from sqlalchemy import (
     Boolean,
     Float,
@@ -54,6 +55,7 @@ from sqlalchemy.orm import (
 from sqlalchemy.sql import expression
 
 from airflow import settings
+from airflow._shared.observability.traces import start_debug_span
 from airflow._shared.timezones import timezone
 from airflow.assets.evaluation import AssetEvaluator
 from airflow.configuration import conf as airflow_conf
@@ -623,6 +625,7 @@ class DagModel(Base):
         return any_deactivated
 
     @classmethod
+    @start_debug_span("dag.dagmodel.dags_needing_dagruns")
     def dags_needing_dagruns(cls, session: Session) -> tuple[Any, dict[str, datetime]]:
         """
         Return (and lock) a list of Dag objects that are due to create a new DagRun.
@@ -746,6 +749,8 @@ class DagModel(Base):
             .order_by(cls.next_dagrun_create_after)
             .limit(cls.NUM_DAGS_PER_DAGRUN_QUERY)
         )
+
+        trace.get_current_span().set_attribute("airflow.dag.asset_triggered_dags", len(triggered_date_by_dag))
 
         return (
             session.scalars(with_row_locks(query, of=cls, session=session, skip_locked=True)),
